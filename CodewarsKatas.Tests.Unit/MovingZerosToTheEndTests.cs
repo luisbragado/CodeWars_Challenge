e87c62a3c09658960fe0631efed20cdf5923d6f6
lbragado <luis.bragado.mx@gmail.com>
2022-04-21 21:02:46 -0500

    Kata - Algoritmo para mover todos los ceros a la izquierda manteniendo el mismo orden en los valores mayores a cero

---
+++ CodewarsKatas.Tests.Unit/MovingZerosToTheEndTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace CodewarsKatas.Tests.Unit
+{
+    [TestFixture]
+    public class MovingZerosToTheEndTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(new int[] { 1, 2, 1, 1, 3, 1, 5, 0, 0, 0, 0 }, MovingZerosToTheEnd.MoveZeroes(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 5 }));
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,9 +10,13 @@ namespace ConsoleApp
             //string result = CodewarsKatas.KataDuplicateEncode.DuplicateEncode("Success");
             //Console.WriteLine(String.Format("Result: {0}", result));
 
-            //Kata: Find Odd Int
-            var result = CodewarsKatas.FindOdd.find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 });
-            Console.WriteLine(String.Format("Result: {0}", result));            
+            ////Kata: Find Odd Int
+            //var result = CodewarsKatas.FindOdd.find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 });
+            //Console.WriteLine(String.Format("Result: {0}", result));            
+
+            //Kata: Moving Zeros to the End
+            var result = CodewarsKatas.MovingZerosToTheEnd.MoveZeroes(new[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 });
+            Console.WriteLine(String.Format("Result: {0}", result));
         }
     }
 }
